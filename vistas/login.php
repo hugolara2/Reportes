70f822c58ec60b3088ddf5a9dccf713ae583e26e
Hugo Lara <hugo.alex200197@outlook.com>
2020-08-01 19:47:28 -0500

    Fotos para logo

---
+++ vistas/login.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="login.css">
+    <title>Iniciar Sesión</title>
+</head>
+<body>
+    <header class="header">
+        <img class="header__img" src="../Photos/3_White_logo_on_color1_243x69.png" alt="Logo">
+    </header>
+    <section class="login">
+        <section class="login__container">
+            <h2>Iniciar Sesión</h2>
+            <form action="../Modelos/login.php" method="POST" class="login__container--form">
+                <label for="user">Usuario</label>
+                <input type="text" class="input" name="user">
+                <label for="pwd">Contraseña</label>
+                <input type="password" class="input" name="pwd">
+                <input type="submit" class="input">
+                <div class="login__container--recordar">
+                    <label> <input type="checkbox" id="cboxRecordar" value="checkbox">
+                        Recuerdame
+                    </label>
+                    <a href="">Olvide mi contraseña</a>
+                </div>
+            </form>
+        </section>
+    </section>
+    <footer class="footer">
+        <a href="">Terminos de uso</a>
+        <a href="">Declarasion de privacidad</a>
+        <a href="">Centro de ayuda</a>
+    </footer>
+</body>
+</html>
