76bd09a5e18d33e248b7b5f31dc882bd2ea4cc54
Hugo Lara <hugo.alex200197@outlook.com>
2020-08-11 17:55:36 -0500

    Constraint personal

--- BaseDeDatos/reportes.sql
+++ BaseDeDatos/reportes.sql
@@ -5,7 +5,7 @@ use Reportes;
 CREATE TABLE Area(
     Id_Area INT NOT NULL,
     Nombre_Area VARCHAR(20) not null,
-    CONSTRAINT pk_area PRIMARY KEY(Id_Area, Nombre_Area)
+    CONSTRAINT pk_area PRIMARY KEY(Id_Area)
 );
 
 CREATE TABLE Personal(
@@ -14,18 +14,15 @@ CREATE TABLE Personal(
     Apellido_Personal VARCHAR(50) NOT NULL,
     Correo_Personal VARCHAR(50) NOT NULL,
     Id_Area INT NOT NULL,
-    Nombre_Area VARCHAR(20) NOT NULL,
-    Puesto_Persona VARCHAR(15) NOT NULL,
-    Entrada_Persona  TIME NOT NULL,
-    Salidsa_Persona TIME NOT NULL,
-    CONSTRAINT pk_persona PRIMARY KEY(Id_Personal, Nombre_Personal, Apellido_Personal),
-    CONSTRAINT fk_persona FOREIGN KEY(Id_Area, Nombre_Area) REFERENCES Area(Id_Area, Nombre_Area)
+    Puesto_Personal VARCHAR(15) NOT NULL,
+    Entrada_Personal  TIME NOT NULL,
+    Salida_Personal TIME NOT NULL,
+    CONSTRAINT pk_persona PRIMARY KEY(Id_Personal),
+    CONSTRAINT fk_persona FOREIGN KEY(Id_Area) REFERENCES Area(Id_Area)
 );
 
 CREATE TABLE Info_Extra(
     Id_Personal BIGINT NOT NULL,
-    Nombre_Personal VARCHAR(50) NOT NULL,
-    Apellido_Personal VARCHAR(50) NOT NULL,
     Domicilio_Personal VARCHAR(50) NOT NULL,
     Municipio VARCHAR(100) NOT NULL,
     Estado VARCHAR(30) NOT NULL,
@@ -33,6 +30,6 @@ CREATE TABLE Info_Extra(
     RFC VARCHAR(15) NOT NULL,
     Seguro VARCHAR(20) NOT NULL,
     CURP VARCHAR(20) NOT NULL,
-    CONSTRAINT fk_Info FOREIGN KEY(Id_Personal, Nombre_Personal, Apellido_Personal) REFERENCES  Persona(Id_Personal, Nombre_Personal, Apellido_Personal)
+    CONSTRAINT fk_Info FOREIGN KEY(Id_Personal) REFERENCES  Persona(Id_Personal)
 );
 
