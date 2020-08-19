d37f55162e32acf442c0f3bd0e9f3522fe5d5af7
Hugo Lara <hugo.alex200197@outlook.com>
2020-08-19 18:14:28 -0500

    Cambios en base de datos y login.php

--- controlador/login.php
+++ controlador/login.php
@@ -9,10 +9,10 @@
 
         echo $usuario." ".$contra;
 
-        $sql = "SELECT* FROM Usuario WHERE Usuario = $usuario AND Contrasenia = $contra";
+        $sql = "SELECT* FROM Usuarios WHERE Usuario = '$usuario' AND Passwrd = '$contra'";
         $result = mysqli_query($conn, $sql);    
         $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
-        $activo = $row['Activo'];
+        $activo = $row['active'];
 
         $count = mysqli_num_rows($result);
 
