f8d16565d0c5b1a74ab00f50a4f8acd357dec52b
Hugo Lara <hugo.alex200197@outlook.com>
2020-08-19 09:59:42 -0500

    Se cambio login.php de controlador

--- controlador/login.php
+++ controlador/login.php
@@ -1,3 +1,10 @@
 <?php   
-    
+    include_once 'conection.php';
+
+    session_start();
+
+    if($_SERVER["REQUEST_METHOD"] == "POST"){
+        $usuario = mysqli_real_escape_string($conn, $_POST[])
+    }
+
 ?>
