06d977aa419cbd82268a5ce5f9c65b33700a2d34
Hugo Lara <hugo.alex200197@outlook.com>
2020-08-19 18:19:15 -0500

    Se borro todo

--- controlador/login.php
+++ controlador/login.php
@@ -1,28 +1,5 @@
 <?php   
-    include 'conection.php';
 
-    if($_SERVER["REQUEST_METHOD"] == "POST"){
-        $usuario = mysqli_real_escape_string($conn, $_POST['usuario']);
-        $contra = mysqli_real_escape_string($conn, $_POST['passwrd']);
 
-        $sql = "SELECT Usuario FROM Usuarios WHERE Usuario = '$usuario' AND Passwrd = '$contra'";
-        $query = mysqli_query($conn, $sql);
-        $result = mysqli_num_rows($query);
-
-        echo $result;
-
-        $sql = "SELECT* FROM Usuarios WHERE Usuario = '$usuario' AND Passwrd = '$contra'";
-        $result = mysqli_query($conn, $sql);    
-        $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
-        $activo = $row['active'];
-
-            header('Location: recuperar.php');
-
-        }
-        else{
-
-        }
-
-    }
 
 ?>
