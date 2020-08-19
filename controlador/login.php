b5ce70401322332032271e3a25b204ab75120a3a
Hugo Lara <hugo.alex200197@outlook.com>
2020-08-19 14:20:34 -0500

    Query

--- controlador/login.php
+++ controlador/login.php
@@ -9,9 +9,11 @@
 
         echo $usuario." ".$contra;
 
-        $sql = "SELECT usuario, contrasenia FROM Usuario WHERE Usuario = $usuario AND contrasenia = $contra";
-        $result = mysqli_query($conn, $sql);
+        $sql = "SELECT* FROM Usuario WHERE Usuario = $usuario AND Contrasenia = $contra";
+        $result = mysqli_query($conn, $sql);    
         $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
+        $activo = $row['Activo'];
+
         $count = mysqli_num_rows($result);
 
         if($count == 1){
